62b25f55c1e7bf8d305f6ad796d66580188c5268
Jędrzej Kuszewski <kuszkuszewski@gmail.com>
2020-02-13 13:41:33 +0100

    Rename ProductBacklog class to just Backlog

---
+++ AgileRunner/Backlog.cs
@@ -0,0 +1,44 @@
+﻿using AgileRunnerAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgileRunner
+{
+	public class Backlog : IExtendableList
+	{
+		private string label;
+		private List<IForm> backlogItems;
+		public Backlog()
+		{
+			this.label = "Backlog";
+			this.backlogItems = new List<IForm>();
+		}
+
+		public List<IForm> GetListElements() => backlogItems;
+
+		public string GetListLabel() => label;
+
+		public IForm GetNewItemForm()
+		{
+			IForm newBacklogItem = new BacklogItem();
+			backlogItems.Add(newBacklogItem);
+			return newBacklogItem;
+		}
+
+		public void RemoveItem(IForm item) => backlogItems.Remove(item);
+		//public void RemoveItem(string label)
+		//{
+		//	foreach(var item in backlogItems)
+		//	{
+		//		var itemInputs = item.GetFormInputs();
+		//		if(itemInputs.ContainsKey(label))
+		//		{
+		//			backlogItems.Remove(item);
+		//		}
+		//	}
+		//}
+	}
+}
